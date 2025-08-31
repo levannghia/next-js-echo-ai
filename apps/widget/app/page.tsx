b6a6d576c34bc273e97b3bc3f92f398655515643
levannghia <meoem2712@gmail.com>
2025-08-31 13:43:50 +0700

    first commit

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,19 +1,15 @@
 'use client'
 
-import {useMutation, useQuery} from 'convex/react'
+import {useQuery} from 'convex/react'
 import {api} from '@workspace/backend/_generated/api'
-import { Button } from '@workspace/ui/components/button'
 
 export default function Page() {
   const users = useQuery(api.user.getMany)
-  const addUser = useMutation(api.user.add)
-
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <p>App/Web</p>
-        <Button onClick={() => addUser()}>Add User</Button>
+        <p>App/Widget</p>
         {users?.map(user => (
           <ul key={user._id}>
             <li>{user.name}</li>
